fd505a8f0c2b4e5b46b2aab7aae963b8915b6c90
Lucas Girardot <lucas.girardot@axn.fr>
2015-10-29 12:39:45 +0100

    oups

--- resources/views/bootstrap3.blade.php
+++ resources/views/bootstrap3.blade.php
@@ -1,5 +1,5 @@
 
-<div class="alert alert-{!! $type !!}" role="alert">
+<div class="alert alert-{!! ($type === 'error' ? 'danger' : $type) !!}" role="alert">
     @if (!empty($title))
         <strong>{{ $title }}</strong><br>
     @endif
