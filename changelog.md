f631d3e2126f23cded544bef887137c7b7a1cf5c
Vincent Garnier <forxer@gmail.com>
2015-10-29 15:27:44 +0100

    Ajout jQuery document ready au template PNotify

--- changelog.md
+++ changelog.md
@@ -1,6 +1,11 @@
 # Changelog for Laravel Notifier
 
 
+1.0.1
+-----
+
+- Ajout jQuery document ready au template PNotify
+
 1.0.0
 -----
 
--- resources/views/pnotify.blade.php
+++ resources/views/pnotify.blade.php
@@ -1,10 +1,12 @@
 
 <script>
-    new PNotify({
-        type: '{{ $type }}',
-        @if (!empty($title))
-            title: '{{ $title }}',
-        @endif
-        text: '{{ $message }}'
+    jQuery(function(){
+        new PNotify({
+            type: '{{ $type }}',
+            @if (!empty($title))
+                title: '{{ $title }}',
+            @endif
+            text: '{{ $message }}'
+        });
     });
 </script>
